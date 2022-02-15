c16c39cb049392fa05910795294d68740c3d40fe
Ziya <ziyagunay06@gmail.com>
2022-02-15 16:36:37 +0100

    Some Web Element Updated

--- src/test/java/com/sprint3/pages/MainPage.java
+++ src/test/java/com/sprint3/pages/MainPage.java
@@ -1,5 +1,58 @@
 package com.sprint3.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
 public class MainPage {
+
+    @FindBy (xpath = "//*[@id='left-menu-settings']")
+    public WebElement Configure;
+
+
+    @FindBy(xpath = "//span[.='Add custom menu item']")
+    public WebElement AddCustomMenu;
+
+    @FindBy(xpath = "//span[.='Collapse menu']")
+    public WebElement CoolapseMenu;
+
+    @FindBy(id = "left-menu-resizer-button")
+    public WebElement CoolapseCheckButton;
+
+
+    @FindBy(xpath = "//span[.='Reset menu']")
+    public WebElement ResetMenu;
+
+    @FindBy(name = "menuPageToFavoriteName")
+    public WebElement NameInputBox;
+
+    @FindBy(name = "menuPageToFavoriteLink")
+    public WebElement LinkInputBox;
+
+    @FindBy(xpath= "//span[.='Add']")
+    public WebElement AddButton;
+
+    @FindBy (id = "menuOpenInNewPage")
+    public WebElement OpenInTabCheckBox;
+
+    @FindBy (xpath = "//a/span[contains(.,'Google')][@class='menu-item-link-text']")
+    public WebElement AddedMenu;
+
+@FindBy(xpath = "//span[starts-with(@class,'menu-fav-editable-btn')]")
+    public List<WebElement>  EditMenu;
+
+
+
+     //span[contains(.,'Hide item')][@class='menu-popup-item-text']   hide
+
+    //span[contains(.,'Delete custom item')][@class='menu-popup-item-text']   delete
+
+    //span[contains(.,'Edit')][@class='menu-popup-item-text']   edit
+             //span[contains(.,'Edit')][@class='popup-window-titlebar-text']
+
+    //span[contains(.,'More')][@class='menu-favorites-more-text']   More
+
+
 
 }
---
+++ src/test/resources/features/ConfigureMenu.feature
@@ -0,0 +1,7 @@
+Feature: User should be able to add custom menu item as Google to redirect google.com
+  Background:
+    Given the user is on the login page
+    Then Verify the user
+
+    Scenario:
+      Given
